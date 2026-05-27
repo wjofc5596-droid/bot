6f0feab45a35ccd7e7af8bab9cdc0788fc6e4708
wjofc5596-droid <wjofc5596@gmail.com>
2026-05-27 18:53:45 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBgIROe4i7jjXTHBubIFEjNvxuaTfzGuao",
+  authDomain: "painel-log.firebaseapp.com",
+  projectId: "painel-log",
+  storageBucket: "painel-log.firebasestorage.app",
+  messagingSenderId: "460411586537",
+  appId: "1:460411586537:web:395a73d7cd5e30ead8ed78",
+  measurementId: "G-F9XLSLN5V2"
+};
+
+export default firebaseConfig;
